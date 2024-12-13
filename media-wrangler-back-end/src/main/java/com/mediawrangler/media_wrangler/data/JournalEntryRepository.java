ed7176d5fc7030d2f7984384e094a526f55361d0
Carddera1842 <carddera1842@gmail.com>
2024-12-12 21:01:38 -0600

    Put the default app back

--- media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/data/JournalEntryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mediawrangler.media_wrangler.data;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JournalEntryRepository extends CrudRepository<JournalEntry, Integer> {
-}

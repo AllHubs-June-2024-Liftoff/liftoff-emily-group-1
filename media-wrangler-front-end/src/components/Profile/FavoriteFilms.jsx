537c1522cdfde4ba31d2227e274e43ef14b26b08
mpres08 <158608642+mpres08@users.noreply.github.com>
2025-01-10 12:54:40 -0600

    poster cards render on user profile

--- media-wrangler-front-end/src/components/Profile/FavoriteFilms.jsx
+++ media-wrangler-front-end/src/components/Profile/FavoriteFilms.jsx
@@ -1,32 +1,11 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import PosterCard from "../MoviePosterCard/PosterCard";
 
-export default function MediaCard() {
+export default function FavoriteFilms() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <div>
+      <h3>Favorite Films</h3>
+      <PosterCard />
+    </div>
   );
 }
--- media-wrangler-front-end/src/components/Profile/Profile.jsx
+++ media-wrangler-front-end/src/components/Profile/Profile.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useAuth } from "../../Services/AuthContext";
 import ProfileHeader from "./ProfileHeader";
 import FavoriteFilms from "./FavoriteFilms";
+import PosterCard from "../MoviePosterCard/PosterCard";
 
 const Profile = () => {
     const { userId } = useParams();
@@ -15,6 +16,7 @@ const Profile = () => {
         <div>
             <ProfileHeader user={user} />
             <FavoriteFilms />
+            
         </div>
     )
 };
